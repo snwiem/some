17170bcc50cca22ab59f8b6d2078e61caac7e1f3
Sebastian Wiemer <snwiem@googlemail.com>
2024-10-09 22:07:24 +0200

    chore: configured vite to output dist to target!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,5 +12,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    outDir: 'target/dist',
+    assetsDir: 'static'
   }
 })
